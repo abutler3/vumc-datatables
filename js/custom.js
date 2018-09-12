1056678bf7d8eb391c1d5a84f20aba341a9b5a27
Andrew Butler <abutler3@gmail.com>
2018-09-12 15:19:35 -0500

    add responsive

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,9 @@
   // jQuery('#tablefield-0').dataTable();
   jQuery(document).ready(function($) {
     if ($('table.tablefield').length > 0 ){
-      jQuery('table.tablefield').dataTable();
+      jQuery('table.tablefield').dataTable(
+      	responsive: true
+      );
     }
   });
 
